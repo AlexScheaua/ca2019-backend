983bc52d5dfebcdd5490eb7c38792dac0148e55c
Alex Scheaua <alex.scheaua22@gmail.com>
2019-06-08 17:58:43 +0300

    form1 JSON adresa din radiobutton

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const formFeedback = require('./models/formfeedback');
+const formIncident = require('./models/formincident');
 const app = express();
 
-// mongoose.connect('mongodb+srv://general:general@cluster0-yu4bb.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
 app.use(express.static(path.join(__dirname, './static')));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb+srv://general:general@cluster0-yu4bb.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+}).catch((error)=> {
+    console.log(error);
+    
+})
 
 //Allow cors
-app.all('*', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    next();
-});
+app.use(cors());
+app.options('*', cors());
 
 app.get('/api/hospitallist', (req, res) => {
     let hList = require('./src/listaSpitale.json');
@@ -22,18 +28,18 @@ app.get('/api/hospitallist', (req, res) => {
 
 app.get('/api/form/:form', (req, res) => {
     console.log(req.params.form);
-    
-    if(req.params.form == 1){
+
+    if (req.params.form == 1) {
         let form = require('./src/form1.json');
         res.status(200).send(form);
-    } else if(req.params.form == 2){
+    } else if (req.params.form == 2) {
         let form = require('./src/form2.json');
         res.status(200).send(form);
     }
-    
+
 })
 
-app.post('/api/form/:form', (req,res) => {
+app.post('/api/form/:form', (req, res) => {
 
 })
 
