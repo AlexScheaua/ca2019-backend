14ca9915961b429c82944b46172f98946e1228f7
Alex Scheaua <alex.scheaua22@gmail.com>
2019-06-08 19:45:29 +0300

    get 2nd form done

--- server.js
+++ server.js
@@ -1,47 +1,106 @@
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
+const MongoClient = require('mongodb')
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const formFeedback = require('./models/formfeedback');
-const formIncident = require('./models/formincident');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, './static')));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+const mongoUrl = 'mongodb+srv://general:general@cluster0-yu4bb.mongodb.net/test?retryWrites=true&w=majority'
 
-mongoose.connect('mongodb+srv://general:general@cluster0-yu4bb.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true
-}).catch((error)=> {
-    console.log(error);
-    
-})
+MongoClient.connect(mongoUrl, {
+  useNewUrlParser: true
+}, (err, db) => {
+  if (err) {
+    console.log(err);
+    process.exit(0);
+  }
+
+  let database = db.db('root');
+  database.createCollection('formfeedback', (err, result) => {
+    if (err) {
+      console.log(err);
+      process.exit(0);
+    }
+
+    db.close();
+  });
+  database.createCollection('formincident', (err) => {
+    if (err) {
+      console.log(err);
+      process.exit(0);
+    }
+    db.close();
+  });
+});
 
 //Allow cors
 app.use(cors());
 app.options('*', cors());
 
 app.get('/api/hospitallist', (req, res) => {
-    let hList = require('./src/listaSpitale.json');
-    res.status(200).send(hList);
+  let hList = require('./src/listaSpitale.json');
+  res.status(200).send(hList);
 })
 
 app.get('/api/form/:form', (req, res) => {
-    console.log(req.params.form);
+  console.log(req.params.form);
 
-    if (req.params.form == 1) {
-        let form = require('./src/form1.json');
-        res.status(200).send(form);
-    } else if (req.params.form == 2) {
-        let form = require('./src/form2.json');
-        res.status(200).send(form);
-    }
-
-})
-
-app.post('/api/form/:form', (req, res) => {
+  if (req.params.form == 1) {
+    let form = require('./src/form1.json');
+    res.status(200).send(form);
+  } else if (req.params.form == 2) {
+    let form = require('./src/form2.json');
+    res.status(200).send(form);
+  }
 
 })
+
+app.post('/api/form/1', (req, res) => {
+  res.send(req.body);
+  // MongoClient.connect(mongoUrl, {
+  //   useNewUrlParser: true
+  // }, (err, db) => {
+  //   if (err) {
+  //     console.log(err);
+  //     process.exit(0);
+  //   }
+  //   var database = db.db('root');    
+  //   var collection = database.collection('formfeedback');
+  //   collection.insertMany(req.body, (err, result) => {
+  //       if(err) {
+  //           console.log(err);
+  //           process.exit(0);
+  //       }
+  //       db.close();
+  //   });
+  // });
+  
+});
+
+app.post('/api/form/2', (req, res) => {
+  res.send(req.body);
+  // MongoClient.connect(mongoUrl, {
+  //   useNewUrlParser: true
+  // }, (err, db) => {
+  //   if (err) {
+  //     console.log(err);
+  //     process.exit(0);
+  //   }
+  //   var database = db.db('root');    
+  //   var collection = database.collection('formincident');
+  //   collection.insertMany(req.body, (err, result) => {
+  //       if(err) {
+  //           console.log(err);
+  //           process.exit(0);
+  //       }
+  //       db.close();
+  //   });
+  // });
+  
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port: ${port}`));
